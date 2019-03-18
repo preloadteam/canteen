589f27c40b6ccbcb25cf4321a8d8c7c349683e2e
Anton Dmitriev <ant-dmitriev@yandex-team.ru>
2019-03-18 23:45:53 +0300

    react init

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,18 @@
-const a = 5;
-console.log(a);
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+
+import '../src/static/_reset.scss';
+
+const store = configureStore({});
+
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    ),
+    document.getElementById('root')
+);
